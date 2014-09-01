3ab847c37036c3863e97ddd2447d22eab1d9ddd3
Daniel Streit <Daniel.Streit@gmail.com>
2014-08-31 17:58:53 -0700

    Test with express

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var tradeEmitter = require('./tradeEmitter');
-var server = require('http').Server();
+var app = require('express')();
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
